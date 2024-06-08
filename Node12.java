2ea16ab21715154be8c2d62809316fa263680705
hunien <huniens06@gmail.com>
2024-06-08 10:48:03 +0700

    first commit

---
+++ Node12.java
@@ -0,0 +1,18 @@
+/**
+ * Node12
+ */
+public class Node12 {
+
+    int data;
+    Node21 left;
+    Node21 right;
+
+    public Node12(){
+
+    }
+    public Node12(int data){
+        this.left = null;
+        this.right = null;
+        this.data = data;
+    }
+}
